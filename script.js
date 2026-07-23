82f4016ddf4dff8a104e0b6d431e5d21b17c5b97
Tihami Rahman <138518399+TihamiDev@users.noreply.github.com>
2026-07-24 00:20:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const volume = document.getElementById("volume");
 
 
 function volume_sphere() {
+	event.preventDefault();
     //Write your code here
 	let r = Number(radius.value);
 	if (r < 0 || isNaN(r)) {
@@ -14,4 +15,5 @@ function volume_sphere() {
   
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+// window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+document.getElementById("MyForm").onsubmit = volume_sphere;
